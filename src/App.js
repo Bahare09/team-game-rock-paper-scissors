50e5691f5c0525bcba47638914847cf2b4bea4c5
Bahare <itxbb09@gmail.com>
2023-04-25 12:02:51 +0100

    Implement Rock, Paper, Scissors game using React (without CSS)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const choices = [{ name: "Rock" }, { name: "Paper" }, { name: "Scissors" }];
+  const [user, setUser] = useState("");
+  const [computer, setComputer] = useState("");
+  const [result, setResult] = useState("");
+  const [userPoint, setUserPoint] = useState(0);
+  const [compPoint, setCompPoint] = useState(0);
+
+  const handleClick = (userChoice) => {
+    setUser(userChoice);
+    const computerChoice =
+      choices[Math.floor(Math.random() * choices.length)].name;
+    setComputer(computerChoice);
+    if (
+      (userChoice === "Rock" && computerChoice === "Scissors") ||
+      (userChoice === "Paper" && computerChoice === "Rock") ||
+      (userChoice === "Scissors" && computerChoice === "Paper")
+    ) {
+      setResult("You Win!!");
+      setUserPoint(userPoint + 1);
+    } else if (userChoice === computerChoice) {
+      setResult("**Tie**");
+    } else {
+      setResult("Computer Wins!");
+      setCompPoint(compPoint + 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home">
+      <div className="container">
+        <h1>Rock,Paper,Scissors Game!</h1>
+        <div className="buttons">
+          {choices.map((choice, index) => (
+            <button key={index} onClick={() => handleClick(choice.name)}>
+              {choice.name}
+            </button>
+          ))}
+        </div>
+        <h3>Your Choice Is :{user}</h3>
+        <h3>Computer Choice Is :{computer}</h3>
+        <h1>{result}</h1>
+        <div className="buttons-1">
+          <div className="col">
+            <h2>User Points:</h2>
+            <button>{userPoint}</button>
+          </div>
+          <div className="col">
+            <h2>Computer Points:</h2>
+
+            <button>{compPoint}</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
