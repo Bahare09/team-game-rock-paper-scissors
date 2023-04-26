ab0aec3b82c7bd49fe13a944a1067c422140fdc4
Bahare <itxbb09@gmail.com>
2023-04-26 20:49:23 +0100

    Game Component

--- src/Game.js
+++ src/Game.js
@@ -1,59 +1,98 @@
-import { useState, React } from "react";
+import { useState } from "react";
+import "./App.css";
 
-export default function Game() {
-  const choices = [{ name: "Rock" }, { name: "Paper" }, { name: "Scissors" }];
-  const [user, setUser] = useState("");
-  const [computer, setComputer] = useState("");
-  const [result, setResult] = useState("");
+function App() {
+  const choices = [
+    { name: "Rock", icon: "rock-icon" },
+    { name: "Paper", icon: "paper-icon" },
+    { name: "Scissors", icon: "scissors-icon" },
+  ];
+  const [user, setUser] = useState("close-hand-icon");
+  const [computer, setComputer] = useState("close-hand-icon");
+  const [result, setResult] = useState("VS");
   const [userPoint, setUserPoint] = useState(0);
   const [compPoint, setCompPoint] = useState(0);
+  const [compHead, setCompHead] = useState([
+    "head-icon **",
+    "head-icon **",
+    "head-icon **",
+  ]);
+  const [userHead, setUserHead] = useState([
+    "head-icon **",
+    "head-icon **",
+    "head-icon **",
+  ]);
 
   const handleClick = (userChoice) => {
-    setUser(userChoice);
-    const computerChoice =
-      choices[Math.floor(Math.random() * choices.length)].name;
-    setComputer(computerChoice);
+    setUser(userChoice.icon);
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    setComputer(computerChoice.icon);
     if (
-      (userChoice === "Rock" && computerChoice === "Scissors") ||
-      (userChoice === "Paper" && computerChoice === "Rock") ||
-      (userChoice === "Scissors" && computerChoice === "Paper")
+      (userChoice.name === "Rock" && computerChoice.name === "Scissors") ||
+      (userChoice.name === "Paper" && computerChoice.name === "Rock") ||
+      (userChoice.name === "Scissors" && computerChoice.name === "Paper")
     ) {
       setResult("You Win!!");
       setUserPoint(userPoint + 1);
+      if (userPoint === 0) {
+        setUserHead(["red-head-icon **", "head-icon **", "head-icon **"]);
+      }
+      if (userPoint === 1) {
+        setUserHead(["red-head-icon **", "red-head-icon **", "head-icon **"]);
+      }
+      if (userPoint === 2) {
+        setUserHead([
+          "red-head-icon **",
+          "red-head-icon **",
+          "red-head-icon **",
+        ]);
+      }
     } else if (userChoice === computerChoice) {
       setResult("**Tie**");
     } else {
       setResult("Computer Wins!");
       setCompPoint(compPoint + 1);
+      if (compPoint === 0) {
+        setCompHead(["red-head-icon **", "head-icon **", "head-icon **"]);
+      }
+      if (compPoint === 1) {
+        setCompHead(["red-head-icon **", "red-head-icon **", "head-icon **"]);
+      }
+      if (compPoint === 2) {
+        setCompHead([
+          "red-head-icon **",
+          "red-head-icon **",
+          "red-head-icon **",
+        ]);
+      }
     }
   };
 
   return (
     <div className="home">
       <div className="container">
-        <h1>Rock,Paper,Scissors Game!</h1>
+        <h1>Rock,Paper,Scissors </h1>
+        <div> {user}</div>
+        <h1>{result}</h1>
+        <div>{computer}</div>
         <div className="buttons">
           {choices.map((choice, index) => (
-            <button key={index} onClick={() => handleClick(choice.name)}>
+            <button key={index} onClick={() => handleClick(choice)}>
               {choice.name}
             </button>
           ))}
         </div>
-        <h3>Your Choice Is :{user}</h3>
-        <h3>Computer Choice Is :{computer}</h3>
-        <h1>{result}</h1>
-        <div className="buttons-1">
-          <div className="col">
-            <h2>User Points:</h2>
-            <button>{userPoint}</button>
-          </div>
-          <div className="col">
-            <h2>Computer Points:</h2>
-
-            <button>{compPoint}</button>
-          </div>
+        <div className="scoreboard">
+          <h1>username</h1>
+          <div>{userHead}</div>
+        </div>
+        <div className="scoreboard">
+          <h1>computer</h1>
+          <div>{compHead}</div>
         </div>
       </div>
     </div>
   );
 }
+
+export default App;
