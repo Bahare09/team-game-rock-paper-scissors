dfab22b5f80395766bd74518066cdcf14d7934e8
Lorena <lorenacapraru@gmail.com>
2023-04-26 10:53:54 +0100

    Welcome Component Structure
    The structure is done. But no functionality yet.

--- src/App.js
+++ src/App.js
@@ -1,61 +1,14 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
+import Welcome from "./Welcome";
+import Game from "./Game";
 
 function App() {
-  const choices = [{ name: "Rock" }, { name: "Paper" }, { name: "Scissors" }];
-  const [user, setUser] = useState("");
-  const [computer, setComputer] = useState("");
-  const [result, setResult] = useState("");
-  const [userPoint, setUserPoint] = useState(0);
-  const [compPoint, setCompPoint] = useState(0);
-
-  const handleClick = (userChoice) => {
-    setUser(userChoice);
-    const computerChoice =
-      choices[Math.floor(Math.random() * choices.length)].name;
-    setComputer(computerChoice);
-    if (
-      (userChoice === "Rock" && computerChoice === "Scissors") ||
-      (userChoice === "Paper" && computerChoice === "Rock") ||
-      (userChoice === "Scissors" && computerChoice === "Paper")
-    ) {
-      setResult("You Win!!");
-      setUserPoint(userPoint + 1);
-    } else if (userChoice === computerChoice) {
-      setResult("**Tie**");
-    } else {
-      setResult("Computer Wins!");
-      setCompPoint(compPoint + 1);
-    }
-  };
-
   return (
-    <div className="home">
-      <div className="container">
-        <h1>Rock,Paper,Scissors Game!</h1>
-        <div className="buttons">
-          {choices.map((choice, index) => (
-            <button key={index} onClick={() => handleClick(choice.name)}>
-              {choice.name}
-            </button>
-          ))}
-        </div>
-        <h3>Your Choice Is :{user}</h3>
-        <h3>Computer Choice Is :{computer}</h3>
-        <h1>{result}</h1>
-        <div className="buttons-1">
-          <div className="col">
-            <h2>User Points:</h2>
-            <button>{userPoint}</button>
-          </div>
-          <div className="col">
-            <h2>Computer Points:</h2>
-
-            <button>{compPoint}</button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <Game />
+      <Welcome />
+    </>
   );
 }
 
---
+++ src/Welcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Welcome() {
+  return (
+    <div className="main">
+      <h1>Rock, Paper, Scissors</h1>
+      <p>Do you dare to play</p>
+      <p>this game?</p>
+      <button>Play</button>
+    </div>
+  );
+}
