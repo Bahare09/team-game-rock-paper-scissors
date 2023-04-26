ce81de4ee00ee889e2933074b9e8c3e7a070525f
Lorena <lorenacapraru@gmail.com>
2023-04-26 11:40:54 +0100

    Routing added to Login and Game pages
    Wait for Tonny's Login component to complete the routing. It's functional considering the Game component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
 import Welcome from "./Welcome";
+import Game from "./Game";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Welcome />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          {/* <Route path=“/login” element={<Login />} /> */}
+          <Route path="/game" element={<Game />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
