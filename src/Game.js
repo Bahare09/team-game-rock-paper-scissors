de20e75bf06729267a3eeb057247f318bf8cd544
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-04-28 15:36:04 +0100

    Changing images, created Game.css, started styling Game.js, changing Header.js

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import "./App.css";
+import Header from "./Header";
+import "./Game.css";
 
 function Game() {
   //to import the useState name from Welcome Page
@@ -8,31 +10,47 @@ function Game() {
   const location = useLocation();
 
   const choices = [
-    { name: "Rock", icon: "rock-icon" },
-    { name: "Paper", icon: "paper-icon" },
-    { name: "Scissors", icon: "scissors-icon" },
+    { name: "Rock", icon: "rock" },
+    { name: "Paper", icon: "paper" },
+    { name: "Scissors", icon: "scissors" },
   ];
-  const [user, setUser] = useState("close-hand-icon");
-  const [computer, setComputer] = useState("close-hand-icon");
+  const [user, setUser] = useState(
+    <img className="left_hand" src="./media/left_rock.webp" alt="rock hand" />
+  );
+  const [computer, setComputer] = useState(
+    <img className="right_hand" src="./media/right_rock.webp" alt="rock hand" />
+  );
   const [result, setResult] = useState("VS");
   const [userPoint, setUserPoint] = useState(0);
   const [compPoint, setCompPoint] = useState(0);
   const [compHead, setCompHead] = useState([
-    "head-icon **",
-    "head-icon **",
-    "head-icon **",
+    <img src="./media/blue_head.webp" alt="blue head" />,
+    <img src="./media/blue_head.webp" alt="blue head" />,
+    <img src="./media/blue_head.webp" alt="blue head" />,
   ]);
   const [userHead, setUserHead] = useState([
-    "head-icon **",
-    "head-icon **",
-    "head-icon **",
+    <img src="./media/blue_head.webp" alt="blue head" />,
+    <img src="./media/blue_head.webp" alt="blue head" />,
+    <img src="./media/blue_head.webp" alt="blue head" />,
   ]);
   const [showModal, setShowModal] = useState(false);
 
   const handleClick = (userChoice) => {
-    setUser(userChoice.icon);
+    setUser(
+      <img
+        className="left_hand"
+        src={`./media/left_${userChoice.icon}.webp`}
+        alt="hand"
+      />
+    );
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    setComputer(computerChoice.icon);
+    setComputer(
+      <img
+        className="right_hand"
+        src={`./media/right_${computerChoice.icon}.webp`}
+        alt="hand"
+      />
+    );
     if (
       (userChoice.name === "Rock" && computerChoice.name === "Scissors") ||
       (userChoice.name === "Paper" && computerChoice.name === "Rock") ||
@@ -41,16 +59,24 @@ function Game() {
       setResult("You Win!!");
       setUserPoint(userPoint + 1);
       if (userPoint === 0) {
-        setUserHead(["red-head-icon **", "head-icon **", "head-icon **"]);
+        setUserHead([
+          <img src="./media/red_head.webp" alt="red head" />,
+          <img src="./media/blue_head.webp" alt="blue head" />,
+          <img src="./media/blue_head.webp" alt="blue head" />,
+        ]);
       }
       if (userPoint === 1) {
-        setUserHead(["red-head-icon **", "red-head-icon **", "head-icon **"]);
+        setUserHead([
+          <img src="./media/red_head.webp" alt="red head" />,
+          <img src="./media/red_head.webp" alt="red head" />,
+          <img src="./media/blue_head.webp" alt="blue head" />,
+        ]);
       }
       if (userPoint === 2) {
         setUserHead([
-          "red-head-icon **",
-          "red-head-icon **",
-          "red-head-icon **",
+          <img src="./media/red_head.webp" alt="red head" />,
+          <img src="./media/red_head.webp" alt="red head" />,
+          <img src="./media/red_head.webp" alt="red head" />,
         ]);
         setShowModal(true);
       }
@@ -60,53 +86,87 @@ function Game() {
       setResult("Computer Wins!");
       setCompPoint(compPoint + 1);
       if (compPoint === 0) {
-        setCompHead(["red-head-icon **", "head-icon **", "head-icon **"]);
+        setCompHead([
+          <img src="./media/red_head.webp" alt="red head" />,
+          <img src="./media/blue_head.webp" alt="blue head" />,
+          <img src="./media/blue_head.webp" alt="blue head" />,
+        ]);
       }
       if (compPoint === 1) {
-        setCompHead(["red-head-icon **", "red-head-icon **", "head-icon **"]);
+        setCompHead([
+          <img src="./media/red_head.webp" alt="red head" />,
+          <img src="./media/red_head.webp" alt="red head" />,
+          <img src="./media/blue_head.webp" alt="blue head" />,
+        ]);
       }
       if (compPoint === 2) {
         setCompHead([
-          "red-head-icon **",
-          "red-head-icon **",
-          "red-head-icon **",
+          <img src="./media/red_head.webp" alt="red head" />,
+          <img src="./media/red_head.webp" alt="red head" />,
+          <img src="./media/red_head.webp" alt="red head" />,
         ]);
         setShowModal(true);
       }
     }
   };
   const restartGame = () => {
-    setUser("close-hand-icon");
-    setComputer("close-hand-icon");
+    setUser(
+      <img className="left_hand" src="./media/left_rock.webp" alt="rock hand" />
+    );
+    setComputer(
+      <img
+        className="right_hand"
+        src="./media/right_rock.webp"
+        alt="rock hand"
+      />
+    );
     setResult("VS");
     setUserPoint(0);
     setCompPoint(0);
     setShowModal(false);
-    setCompHead(["head-icon **", "head-icon **", "head-icon **"]);
-    setUserHead(["head-icon **", "head-icon **", "head-icon **"]);
+    setCompHead([
+      <img src="./media/blue_head.webp" alt="blue head" />,
+      <img src="./media/blue_head.webp" alt="blue head" />,
+      <img src="./media/blue_head.webp" alt="blue head" />,
+    ]);
+    setUserHead([
+      <img src="./media/blue_head.webp" alt="blue head" />,
+      <img src="./media/blue_head.webp" alt="blue head" />,
+      <img src="./media/blue_head.webp" alt="blue head" />,
+    ]);
   };
 
   return (
     <div className="home">
-      <div className="container">
-        <h1>Rock,Paper,Scissors</h1>
-        <div> {user}</div>
-        <h1>{result}</h1>
-        <div>{computer}</div>
-        <div className="buttons">
-          {choices.map((choice, index) => (
-            <button key={index} onClick={() => handleClick(choice)}>
-              {choice.name}
-            </button>
-          ))}
+      <Header />
+      <div className="game_container">
+        <div className="hands_container">
+          {user}
+          <h1 className="result">{result}</h1>
+          {computer}
         </div>
-        <div className="scoreboard">
-          <h1>{location.state.name}</h1>
-          <div>{userHead}</div>
-        </div>
-        <div className="scoreboard">
-          <h1>computer</h1>
-          <div>{compHead}</div>
+
+        <div className="scoreboards_buttons">
+          <div className="buttons">
+            {choices.map((choice, index) => (
+              <button
+                className="btn"
+                key={index}
+                onClick={() => handleClick(choice)}
+              >
+                {choice.name}
+              </button>
+            ))}
+          </div>
+
+          <div className="scoreboard scoreboard_1">
+            <h1>{location.state.name}</h1>
+            <div className="head_group">{userHead}</div>
+          </div>
+          <div className="scoreboard scoreboard_2">
+            <h1>Computer</h1>
+            <div className="head_group">{compHead}</div>
+          </div>
         </div>
         {showModal && (
           <div className="modal">
--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <div>
       <h1 id="shadow">
-        <span id="glow">Rock, Paper, </span>
-        <span id="blink">Scissors</span>
+        <span id="glow">ROCK, PAPER, </span>
+        <span id="blink">SCISSORS</span>
       </h1>
     </div>
   );
