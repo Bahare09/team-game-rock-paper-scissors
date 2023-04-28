9a4290a1b3bb66b5396a8dec9eaa8d956b20fe48
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-04-28 01:25:06 +0100

    Adding Header component, Header.css, styling Welcome component

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div>
+      <h1 id="shadow">
+        <span id="glow">Rock, Paper, </span>
+        <span id="blink">Scissors</span>
+      </h1>
+    </div>
+  );
+}
